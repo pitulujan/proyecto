f6678540a2ef40adba68e5eb934114430efc8380
pitulujan <lujanmartin91@gmail.com>
2018-10-28 12:17:13 -0300

    cambio de base de datos

--- app/manage_users.py
+++ app/manage_users.py
@@ -1,17 +1,16 @@
-from app.models import User, User_State
-from app import app, db
+from app.models import User, User_Light_State,User_Temperature_State,Scheduled_events
+from app import db
 
 def create_user_full(form):
 
     create_user = User(username=form.username.data, admin=form.admin.data)
     create_user.set_password(form.password.data)
-    db.session.add(create_user)
-    db.session.commit()
+    create_light_state=User_Light_State(user=form.username.data)
+    create_temp_state=User_Temperature_State(user=form.username.data)
 
-    id_us= User.query.filter_by(username=form.username.data).first()
-   
-    create_state = User_State(user_id=id_us.id,light_state=False,light_intensity=10, temp_state=20.0)
-    db.session.add(create_state)
+    db.session.add(create_user)
+    db.session.add(create_temp_state)
+    db.session.add(create_light_state)
     db.session.commit()    
         
     return 'Congratulations, you have just registered '+form.username.data+'!'
@@ -19,9 +18,14 @@ def create_user_full(form):
 def delete_user_full(user):
 
     delete_user = User.query.filter_by(username=user).first()
-    state_to_del= User_State.query.filter_by(user_id=delete_user.id).first()   
+    light_state_to_del= User_Light_State.query.filter_by(user=user).first()   
+    temp_state_to_del= User_Temperature_State.query.filter_by(user=user).first()  
+    shceduled_events_to_del=Scheduled_events.quey.filter_by(user=user)
+    
     db.session.delete(delete_user)
-    db.session.delete(state_to_del)
+    db.session.delete(light_state_to_del)
+    db.session.delete(temp_state_to_del)
+    db.session.delete(shceduled_events_to_del)
     db.session.commit()
 
     return 'Congratulations, you have just deleted '+delete_user.username+ ' from the users list!'
