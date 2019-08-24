c6478edbb421138a8fe8fd62739e9a280c39b6da
pitulujan <lujanmartin91@gmail.com>
2019-08-24 14:07:47 -0300

    temp mod

--- app/server.py
+++ app/server.py
@@ -184,7 +184,7 @@ def touch_switch_mqtt(client,userdata,message):
 ########################################
 broker_address="192.168.2.20"
 #broker_address="127.0.0.1"
-client = mqtt.Client("web_app_pc") #create new instance
+client = mqtt.Client("web_app") #create new instance
 client.will_set("tele/sonoff/LWT", payload="gorda traga leche", qos=0, retain=True)
 client.message_callback_add("tele/sonoff/INFO1", info1_mqtt)
 client.message_callback_add("stat/sonoff/RESULT", result_mqtt)
